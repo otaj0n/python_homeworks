02ff750c42cf157b2a9694d2ec4ef940274d91a5
otaj0n <mustangcamaro001010@gmail.com>
2024-12-13 20:11:59 +0500

    fix

--- lesson_2/homeworks/string_4.py
+++ lesson_2/homeworks/string_4.py
@@ -1,12 +1,2 @@
-def palindrome(l):
-    h = len(l)
-    if h%2 != 0:
-        if l[:h//2]==l[-1:h//2:-1]:
-            return True
-        else:
-            return False
-    else:
-        return False
 l = input()
-
-print(palindrome(l))
+print(l == l[-1::-1])
--- lesson_2/homeworks/string_5.py
+++ lesson_2/homeworks/string_5.py
@@ -1,12 +1,11 @@
 l = input()
 
-vowels = "aeiouAEIOU"
+vowels = "aeiou"
 vowels_count = consonants_count = 0
 for char in l:
-    if char.isalpha():
-        if char in vowels:
-            vowels_count += 1
-        else:
-                consonants_count += 1
+    if char.lower() in vowels:
+        vowels_count += 1
+    else:
+        consonants_count += 1
 
 print(f"Vowels = {vowels_count}, Consonants = {consonants_count}")
